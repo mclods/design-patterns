5a403f53027f31a75486eb95cb0b6c88b18b1f19
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2025-08-31 00:15:17 +0530

    Added tests with verify workflow

--- src/main/java/org/mclods/observer_pattern/WeatherStation.java
+++ src/main/java/org/mclods/observer_pattern/WeatherStation.java
@@ -1,25 +1,29 @@
 package org.mclods.observer_pattern;
 
-import org.mclods.observer_pattern.builtin.observers.impl.CurrentConditionsDisplay;
-import org.mclods.observer_pattern.builtin.observers.impl.StatisticsDisplay;
-import org.mclods.observer_pattern.builtin.subject.WeatherData;
-
 public class WeatherStation {
-    public static void run() {
+    public static void runCustomImpl() {
         // Custom Observer Pattern Implementation
-//        WeatherData weatherData = new WeatherData();
-//
-//        CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
-//        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
-//
-//        weatherData.setMeasurements(30.0, 65.0, 30.4);
-//        weatherData.setMeasurements(28.0, 70.0, 29.2);
+        org.mclods.observer_pattern.custom.subject.impl.WeatherData weatherData =
+                new org.mclods.observer_pattern.custom.subject.impl.WeatherData();
 
+        org.mclods.observer_pattern.custom.observers.impl.CurrentConditionsDisplay currentDisplay =
+                new org.mclods.observer_pattern.custom.observers.impl.CurrentConditionsDisplay(weatherData);
+        org.mclods.observer_pattern.custom.observers.impl.StatisticsDisplay statisticsDisplay =
+                new org.mclods.observer_pattern.custom.observers.impl.StatisticsDisplay(weatherData);
+
+        weatherData.setMeasurements(30.0, 65.0, 30.4);
+        weatherData.setMeasurements(28.0, 70.0, 29.2);
+    }
+
+    public static void runBuiltInImpl() {
         // Built-in Observer Pattern Implementation
-        WeatherData weatherData = new WeatherData();
+        org.mclods.observer_pattern.builtin.subject.WeatherData weatherData =
+                new org.mclods.observer_pattern.builtin.subject.WeatherData();
 
-        CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
-        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        org.mclods.observer_pattern.builtin.observers.impl.CurrentConditionsDisplay currentDisplay =
+                new org.mclods.observer_pattern.builtin.observers.impl.CurrentConditionsDisplay(weatherData);
+        org.mclods.observer_pattern.builtin.observers.impl.StatisticsDisplay statisticsDisplay =
+                new org.mclods.observer_pattern.builtin.observers.impl.StatisticsDisplay(weatherData);
 
         weatherData.setMeasurements(43.0, 67.0, 32.1);
         weatherData.setMeasurements(25.7, 90.0, 29.2);
