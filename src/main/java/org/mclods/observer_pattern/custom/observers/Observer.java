6194211af5d1c424f502cd35df120d69578ab84c
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2025-08-30 12:49:17 +0530

    Refactored Code (#4)
    Add PR Template

--- src/main/java/org/mclods/observer_pattern/custom/observers/Observer.java
+++ src/main/java/org/mclods/observer_pattern/custom/observers/Observer.java
@@ -1,5 +1,5 @@
 package org.mclods.observer_pattern.custom.observers;
 
 public interface Observer {
-    public void update(double temperature, double humidity, double pressure);
+    void update(double temperature, double humidity, double pressure);
 }
--- src/main/java/org/mclods/observer_pattern/custom/subject/Subject.java
+++ src/main/java/org/mclods/observer_pattern/custom/subject/Subject.java
@@ -3,7 +3,7 @@ package org.mclods.observer_pattern.custom.subject;
 import org.mclods.observer_pattern.custom.observers.Observer;
 
 public interface Subject {
-    public void registerObserver(Observer observer);
-    public void removeObserver(Observer observer);
-    public void notifyObservers();
+    void registerObserver(Observer observer);
+    void removeObserver(Observer observer);
+    void notifyObservers();
 }
